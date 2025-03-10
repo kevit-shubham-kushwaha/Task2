577ae8a644024d78b383857f06efb869c601083a
kevit-shubham-kushwaha <shubham.kushwaha@kevit.io>
2025-03-10 11:25:36 +0530

    commit from feature1 branch adding main.py and modifing main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("adding some code here to merge with development branch")
+# with this snippet from Task2/main.py:
+print("hello from main.py")
+# The output of the program is:
+# hello from file.py
